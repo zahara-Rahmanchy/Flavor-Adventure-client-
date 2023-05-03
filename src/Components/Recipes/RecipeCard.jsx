c2466f76f98147057fefb617304af8eb7f22377c
zahara-Rahmanchy <zaharachy72@gmail.com>
2023-05-03 17:03:12 +0600

    add to favourite button added

--- src/Components/Recipes/RecipeCard.jsx
+++ src/Components/Recipes/RecipeCard.jsx
@@ -1,9 +1,28 @@
-import React from "react";
-import {Col, Container, Row, Image, Card, Table} from "react-bootstrap";
+import React, {useState} from "react";
+import {Col, Container, Row, Button, Card, Table} from "react-bootstrap";
+import {BsSuitHeartFill} from "react-icons/bs";
+import {ToastContainer, toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const RecipeCard = props => {
   const {ingredients, cooking_method, recipe_image, rating, recipe_name} =
     props.recipe;
-  console.log(recipe_image);
+  const [click, setClick] = useState(false);
+
+  const handleFavourite = () => {
+    // toast("Wow so easy!");
+    toast.error("Added to Favourites!", {
+      icon: false,
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+    setClick(true);
+  };
   return (
     <>
       <h2 className="mt-3 fw-normal text-center">{recipe_name}</h2>
@@ -14,8 +33,10 @@ const RecipeCard = props => {
               <th className="bg-white" colSpan={3}>
                 Ingredients
               </th>
-              {ingredients.map(i => (
-                <td style={{background: "#FF7B74", color: "white"}}>{i}</td>
+              {ingredients.map((ingredient, index) => (
+                <td style={{background: "#FF7B74", color: "white"}} key={index}>
+                  {ingredient}
+                </td>
               ))}
             </tr>
             <br />
@@ -38,6 +59,27 @@ const RecipeCard = props => {
             </tr>
           </tbody>
         </Table>
+        <Button
+          className="d-flex float-end text-white flex-column rounded border-0"
+          style={{background: "#F4716A", width: "100px"}}
+          onClick={handleFavourite}
+          disabled={click}
+        >
+          <BsSuitHeartFill className="mt-2 fs-3 mx-auto text-white" />
+          <p className="m-0 p-0">Add to Favourites</p>
+        </Button>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+        ;
       </Col>
       <Col>
         <Card className="bg-transparent border-0"></Card>
